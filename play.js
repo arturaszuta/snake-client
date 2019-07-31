c2ee1584bf87f99c5129654acc78833f9c7894b9
Arturas Zuta <arturaszuta@gmail.com>
2019-07-31 16:34:16 +0000

    Split main function into modules

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require('net');
+const { connect } = require('./client');
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '172.46.0.110',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-   
-  conn.on('data', (data) => {
-    console.log(`Server says: ${data}`);
-  })
-  
-  return conn;
-};
+
 
 
 console.log('Connecting ...');
